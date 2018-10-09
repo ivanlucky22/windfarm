d7d95c48c5ba39d444431fdb7537808ffc99b0ce
ivan.steshov <ivan.steshov@b-next.com>
2018-10-09 12:12:10 +0300

    Initial commit

---
+++ src/main/java/com/pexapark/windfarm/WindfarmApplication.java
@@ -0,0 +1,12 @@
+package com.pexapark.windfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindfarmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WindfarmApplication.class, args);
+    }
+}
